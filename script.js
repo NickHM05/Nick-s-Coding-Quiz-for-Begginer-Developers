788f9958f11e7ab350c24a0e8ae64e4d929c04d9
NicholasHM05 <nhmamberger@gmail.com>
2022-11-13 16:04:32 -0600

    added all the question and answer text for use in javascript

---
+++ script.js
@@ -0,0 +1,68 @@
+const quizData = [
+    {
+        question:"What does HTML stand for?",
+        a: "Hyperterminal Max Loader",
+        b: "Hypertext Madeup Language",
+        c: "Hypertext Markup Language",
+        d: "Hybrid Terminal Modification Link",
+        correct: "c",
+    },
+    {
+        question:"What element is a container for all the head elements, and may include the document title, scripts, styles, meta information, and more?",
+        a:"<head></head>",
+        b:"<body></body>",
+        c:"<title></title>",
+        d:"<br></br>",
+        correct:"a",
+    },
+    {
+        question:"What property is used to specify whether or not an element should appear as if it’s on top of or above other content on the page?",
+        a:"Foreground",
+        b:"Float",
+        c:"Jumper",
+        d:"Top Level",
+        correct:"b",
+    },
+    {
+        question:"What are the CSS properties that are used to add space around sections of content?",
+        a:"Break",
+        b:"Spacing",
+        c:"Clear",
+        d:"Padding",
+        correct:"d",
+    },
+    {
+        question:"What is the object called that lets you work with both dates and time-related data?",
+        a:"Dates",
+        b:"Clock",
+        c:"Watch",
+        d:"Time",
+        correct:"a",
+    },
+    {
+        question:"How do functions break up?",
+        a:"They leave the webpage.",
+        b:"They regret their choices.",
+        c:"They work things out.",
+        d:"They stop calling each other!",
+        correct:"d",
+    },
+    {
+        question:"What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?",
+        a:"Function",
+        b:"Loop",
+        c:"Clone",
+        d:"Boolean",
+        correct:"b",
+    },
+    {
+        question:"What is the element used (and hidden) in code that explains things and makes the content more readable?",
+        a:"Quotations",
+        b:"Hidden Elements",
+        c:"Comments",
+        d:"Notes",
+        correct:"c",
+    },
+
+
+];
